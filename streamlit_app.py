8782bffaf18da7788aa826306865cb34e7b6ca8c
Ashmeet Kaur <43696983+MeetashKaur@users.noreply.github.com>
2023-03-29 14:52:56 +0530

    Update streamlit_app.py
    added csv file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥣 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+import pandas 
+my_fruits_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruits_list)
