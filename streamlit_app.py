44cd4dfdb8e5a5093ea499d83464007162bb0bd7
Ashmeet Kaur <43696983+MeetashKaur@users.noreply.github.com>
2023-04-04 14:54:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥣 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
+streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
