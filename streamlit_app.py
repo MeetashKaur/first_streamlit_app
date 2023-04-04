877d8560f6d4184a89d585d75a93e69b6b7ef001
Ashmeet Kaur <43696983+MeetashKaur@users.noreply.github.com>
2023-04-04 14:57:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
 #normalizing the JSON version of the response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
