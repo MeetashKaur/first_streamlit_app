0911dcb82454745684946ac2be2687e37f574084
Ashmeet Kaur <43696983+MeetashKaur@users.noreply.github.com>
2023-04-05 12:19:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.text('🥣 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   #normalizing the JSON version of the response
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   
@@ -21,7 +21,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.write('The user entered ', fruit_choice)
+    streamlit.error('Please select a fruit to get the information.')
   else:
     back_from_function=get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
