193a4dbfc1ef610a82198af2694d9e43c8f1c942
Ashmeet Kaur <43696983+MeetashKaur@users.noreply.github.com>
2023-04-05 12:16:05 +0530

    Update streamlit_app.py
    creating function

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,17 +11,20 @@ streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥣 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  #normalizing the JSON version of the response
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  
 streamlit.header("Fruityvice Fruit Advice!")
-
+  
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.write('The user entered ', fruit_choice)
   else:
-    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    #normalizing the JSON version of the response
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+    
 except UTLError as e:
   streamlit.error()
